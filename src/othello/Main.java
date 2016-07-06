186eb7f1e838adaad16f986b654e88b9f65080c4
Henri Paulamäki <henripau@dhcp-asv-79.helsinki.fi>
2016-07-06 19:55:49 +0300

    test commit

--- src/othello/Main.java
+++ src/othello/Main.java
@@ -12,7 +12,7 @@ public class Main {
     public static void main(String[] args) {
         new GameMaster("textui").run();
         
-        //testing();
+        //testing();//////////////////
     }
     
     public static void testing() {
